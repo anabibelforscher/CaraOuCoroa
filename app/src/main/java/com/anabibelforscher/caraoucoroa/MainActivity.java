cf0620eac79bcdd10dd36fea545521a405facc0a
anabibelforscher <anaolivercontact@gmail.com>
2021-06-28 18:04:42 -0300

    Finalização das telas | Criação de ResultadoActivity

--- app/src/main/java/com/anabibelforscher/caraoucoroa/MainActivity.java
+++ app/src/main/java/com/anabibelforscher/caraoucoroa/MainActivity.java
@@ -2,13 +2,27 @@ package com.anabibelforscher.caraoucoroa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mBotaoJogar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBotaoJogar = findViewById(R.id.botaoJogar);
+        mBotaoJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),ResultadoActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
